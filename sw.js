2230e3902a71f3de05f0c73e6a224a85a4864eb9
Felipe Biava Catâneo <93289514+Felipecataneo@users.noreply.github.com>
2026-01-09 12:05:54 -0300

    Update service worker for cache management
    Updated cache name and added new files to cache. Implemented immediate update and control of clients on activation.

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
-const CACHE_NAME = "dinopepe-v1";
+const CACHE_NAME = "dinopepe-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./fundo.mp3"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting(); // Força a atualização imediata
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim(); // Controla a página imediatamente
 });
 
 self.addEventListener("fetch", event => {
